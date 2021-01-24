d7e1e7e3730845d2c0a8376f15b4491a8175b895
seehearcait <caitlinmichie19@gmail.com>
2021-01-24 12:31:37 -0600

    Add TrackList js and css

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Playlist.css'
 
+import TrackList from '../TrackList/TrackList'
+
 class Playlist extends React.Component {
 
     render() {
         return (
             <div className="Playlist">
                 <input defaultValue={'New Playlist'} />
-                {/* <!-- Add a TrackList component --> */}
+                <TrackList />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
